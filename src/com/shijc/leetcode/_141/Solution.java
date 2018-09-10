47ec8919c6d64c93f18bda39e15fc0fb93c8b00a
shijiacheng <iot_shijiacheng@163.com>
2018-09-10 20:16:08 +0800

    141. 环形链表

---
+++ src/com/shijc/leetcode/_141/Solution.java
@@ -0,0 +1,37 @@
+package com.shijc.leetcode._141;
+
+import com.shijc.leetcode.ListNode;
+
+/**
+ * 141. 环形链表
+ * 给定一个链表，判断链表中是否有环。
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null){
+            return false;
+        }
+
+        ListNode slow = head.next;
+        if (slow == null){
+            return false;
+        }
+
+        ListNode fast = slow.next;
+        while (slow!=null && fast!=null){
+            if (slow == fast){
+                return true;
+            }
+
+            slow = slow.next;
+            fast = fast.next;
+
+            if (fast!= null){
+                fast = fast.next;
+            }
+        }
+
+
+        return false;
+    }
+}
