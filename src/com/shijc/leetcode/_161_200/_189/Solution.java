008f055b3cc06251bc21d93ce44361bc435fd955
shijiacheng <iot_shijiacheng@163.com>
2018-10-24 22:18:18 +0800

    189.旋转数组

---
+++ src/com/shijc/leetcode/_161_200/_189/Solution.java
@@ -0,0 +1,45 @@
+package com.shijc.leetcode._161_200._189;
+
+/**
+ * 189.旋转数组
+ * rotate array
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ *
+ * 输入: [-1,-100,3,99] 和 k = 2
+ * 输出: [3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ */
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if (k>=nums.length){
+            k = k%nums.length;
+        }
+
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+
+
+    private void reverse(int[] nums,int start,int end){
+        while (start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
