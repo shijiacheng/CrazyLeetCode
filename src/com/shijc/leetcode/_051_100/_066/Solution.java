6ae8df98bbd8c48c725c8f1d1ec21be0ebc4e242
shijiacheng <iot_shijiacheng@163.com>
2018-11-04 14:54:25 +0800

    66.加一

---
+++ src/com/shijc/leetcode/_051_100/_066/Solution.java
@@ -0,0 +1,49 @@
+package com.shijc.leetcode._051_100._066;
+
+/**
+ * 66.加一
+ * Plus One
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * <p>
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * <p>
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 示例 2:
+ * <p>
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ *
+ * @author shijiacheng
+ */
+public class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+
+        return newDigits;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] digits = {9};
+        int[] newDigits = s.plusOne(digits);
+        System.out.println(newDigits[0]);
+    }
+}
