4c4a046608264663307dc109e4b1973c12ea8780
shijiacheng <iot_shijiacheng@163.com>
2018-11-20 22:17:48 +0800

    448.找到所有数组中消失的数字

---
+++ src/com/shijc/leetcode/_401_450/_448/Solution.java
@@ -0,0 +1,45 @@
+package com.shijc.leetcode._401_450._448;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 448.找到所有数组中消失的数字
+ * find all numbers disappeared in an array
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ *
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ *
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * 输出:
+ * [5,6]
+ */
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        if (nums == null || nums.length == 0){
+            return list;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index]>0){
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>0){
+                list.add(i+1);
+            }
+        }
+
+        return list;
+    }
+}
