d967cde0e986d1427500026c02368a63a8aa0066
shijiacheng <iot_shijiacheng@163.com>
2018-11-20 22:17:38 +0800

    661.图片平滑器

---
+++ src/com/shijc/leetcode/_651_700/_661/Solution.java
@@ -0,0 +1,61 @@
+package com.shijc.leetcode._651_700._661;
+
+/**
+ * 661.图片平滑器
+ * submissions
+ * 包含整数的二维矩阵 M 表示一个图片的灰度。你需要设计一个平滑器来让每一个单元的灰度成为平均灰度 (向下舍入) ，平均灰度的计算是周围的8个单元和它本身的值求平均，如果周围的单元格不足八个，则尽可能多的利用它们。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * [[1,1,1],
+ *  [1,0,1],
+ *  [1,1,1]]
+ * 输出:
+ * [[0, 0, 0],
+ *  [0, 0, 0],
+ *  [0, 0, 0]]
+ * 解释:
+ * 对于点 (0,0), (0,2), (2,0), (2,2): 平均(3/4) = 平均(0.75) = 0
+ * 对于点 (0,1), (1,0), (1,2), (2,1): 平均(5/6) = 平均(0.83333333) = 0
+ * 对于点 (1,1): 平均(8/9) = 平均(0.88888889) = 0
+ * 注意:
+ *
+ * 给定矩阵中的整数范围为 [0, 255]。
+ * 矩阵的长和宽的范围均为 [1, 150]。
+ */
+public class Solution {
+    public int[][] imageSmoother(int[][] M) {
+
+        if(M == null)return null;
+        int rows = M.length;
+        if(rows == 0)return new int[0][];
+
+        int cols = M[0].length;
+
+        int[][] res = new int[rows][cols];
+
+        for(int row=0;row<rows;row++){
+            for(int col =0;col < cols;col++){
+                int count = 0;
+                int sum = 0;
+                for(int incR : new int[]{-1,0,1}){
+                    for(int incC : new int[]{-1,0,1}){
+                        if(isValid(rows,cols,row+incR,col+incC)){
+                            count++;
+                            sum += M[row+incR][col+incC];
+                        }
+                    }
+                }
+                res[row][col] = sum/count;
+            }
+        }
+
+        return res;
+    }
+
+    public boolean isValid(int row,int col,int x,int y){
+        return x >= 0 && x < row && y >= 0 && y < col;
+
+    }
+}
