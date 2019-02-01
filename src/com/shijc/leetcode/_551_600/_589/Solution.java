cdb1d61f08b65fc4b51fc520f99cb24aa9815bfe
shijiacheng <iot_shijiacheng@163.com>
2019-02-01 21:15:54 +0800

    589. N叉树的前序遍历

---
+++ src/com/shijc/leetcode/_551_600/_589/Solution.java
@@ -0,0 +1,50 @@
+package com.shijc.leetcode._551_600._589;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 589. N叉树的前序遍历
+ * n ary tree preorder traversal
+ * 给定一个 N 叉树，返回其节点值的前序遍历。
+ *
+ * 例如，给定一个 3叉树 :
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * 返回其前序遍历: [1,3,5,6,2,4]。
+ */
+public class Solution {
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null)
+            return res;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            res.add(root.val);
+            for (int i = root.children.size() - 1; i >= 0; i--)
+                stack.push(root.children.get(i));
+        }
+        return res;
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val,List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
